470889767d1a8a064955892aea9b2b4817bbaea1
Leonor Machado <leonor.m.machado@tecnico.ulisboa.pt>
2026-01-10 12:27:34 +0000

    tabuleiro 30x30 pq o stor disse que era boa ideia por

---
+++ SO-2526-sol-parte1/dir3/m1.m
@@ -0,0 +1,6 @@
+# PASSO: numero de jogadas a esperar entre movimentos
+PASSO 10
+# POS: posicao inicial (coluna linha) - (1,1) e um espaco vazio no canto superior esquerdo
+POS 3 3
+# Comandos: R=Random (direcao aleatoria)
+R
---
+++ SO-2526-sol-parte1/dir3/m2.m
@@ -0,0 +1,17 @@
+# PASSO: numero de jogadas a esperar entre movimentos
+PASSO 10
+# POS: posicao inicial (coluna linha) - (28,28) e um espaco vazio no canto inferior direito
+POS 28 28
+# Comandos: W=Cima, A=Esquerda, S=Baixo, D=Direita
+W
+W
+W
+A
+A
+A
+S
+S
+S
+D
+D
+D
